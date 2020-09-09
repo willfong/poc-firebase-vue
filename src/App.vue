5fb1dc17c9706b6f31785e6ca5d10db6f6cf1cbd
Will Fong <willfong@pm.me>
2020-09-09 11:27:55 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+		<p>Hey dude!</p>
+
+  </div>
+</template>
+
+<script>
+
+import { db } from './db'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      documents: [],
+    }
+	},
+	firestore: {
+    documents: db.collection('documents'),
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
